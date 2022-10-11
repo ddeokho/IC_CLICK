5c2b458a6b686bf14783e98771c27520511c1a52
Deokho Kim <sksski1359@gmail.com>
2022-10-12 08:26:49 +0900

    Update click.py

--- click.py
+++ click.py
@@ -1,14 +1,10 @@
 from pynput.mouse import Listener
 
 
-coord = []
-
 def click(x,y, button, pressed) :
 	if pressed:
 		x = int(x)
 		y = int(y)
-		coord.append(x)
-		coord.append(y)
 
 		print('x='+str(x)+' y='+str(y))
 
